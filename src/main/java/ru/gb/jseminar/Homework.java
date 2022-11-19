cdf72c58cb37079a93753222cd80c7e1a73bafbe
irinas <irina.sidor.2000@mail.ru>
2022-11-19 18:25:37 +0300

    add homework

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,30 +1,84 @@
 package ru.gb.jseminar;
 
+import com.sun.source.tree.ContinueTree;
 import ru.gb.jseminar.data.Notebook;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Homework {
 
     // 1) Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
-    // 2) Создать коллекцию ноутбуков.
+    // 2) Создать список ноутбуков.
     // 3) Написать мапу, которая будет содержать критерий (или критерии) фильтрации и выведет
     //      ноутбуки, отвечающие фильтру.
-    //      Пример: ОЗУ - Значение, Объем ЖД - Значение, Операционная система - Значение, Цвет - Значение
+    //      Пример: 1 - ОЗУ, 2 - Объем ЖД, 3 - Операционная система, 4 - Цвет
     // 4) Отфильтровать ноутбуки их первоначального множества и вывести проходящие по условиям.
     public static void main(String[] args) {
+        Homework hw = new Homework();
+
+
+
+        List<Notebook> products = hw.initListNotebooks();
+
+        Map<String, String> filterParam = new TreeMap<>();
+        filterParam.put("color", "black");
+        filterParam.put("ram", "8");
+//        filterParam.put("hd", "1000");
+
+        List<Notebook> FiltredProducts = hw.filter(filterParam, products);
+        hw.printList(FiltredProducts);
+
 
     }
+
 
     public List<Notebook> initListNotebooks(){
 
-        return null;
+        List<Notebook> product = new ArrayList<>();
+
+        Notebook item_0 = new Notebook("1", "8", "1000", "Win10", "black", "amd");
+        Notebook item_1 = new Notebook("2", "16", "2000", "Linux", "white", "amd");
+        Notebook item_2 = new Notebook("3", "32", "3000", "Win10", "pink", "intel");
+        Notebook item_3 = new Notebook("4", "64", "1000", "Linux", "blue", "intel");
+        Notebook item_4 = new Notebook("5", "32", "5000", "Win10", "green", "amd");
+        Notebook item_5 = new Notebook("6", "16", "10000", "Linux", "gray", "intel");
+
+        product.add(item_0);
+        product.add(item_1);
+        product.add(item_2);
+        product.add(item_3);
+        product.add(item_4);
+        product.add(item_5);
+
+        return product;
     }
 
-    public List<Notebook> filter(Map<String, String> params, List<Notebook> notebooks){
+    public List<Notebook> filter(Map<String, String> params, List<Notebook> prod){
 
-        return null;
+        List<Notebook> filtredList = new ArrayList<>();
+
+        for (Notebook item: prod) {
+            List<String> tmp = new ArrayList<>();
+
+            for (String param: params.keySet()) {
+
+                if (item.CreateDict().get(param).equals(params.get(param))){
+                    tmp.add("ok");
+                }
+            }
+            if (tmp.size() == params.size()) {
+                filtredList.add(item);
+            }
+
+        }
+
+        return filtredList;
+    }
+
+    public void printList(List<Notebook> prod){
+        for (Notebook item: prod) {
+            System.out.println(item.CreateDict());
+        }
     }
 
 }
--- src/main/java/ru/gb/jseminar/data/Notebook.java
+++ src/main/java/ru/gb/jseminar/data/Notebook.java
@@ -1,4 +1,92 @@
 package ru.gb.jseminar.data;
 
+import java.util.*;
+
 public class Notebook {
+    private String os, color, model, ram, hd, processor;
+
+    public Notebook (String model, String ram, String hd, String os, String color, String processor) {
+        this.model = model;
+        this.ram = ram;
+        this.hd = hd;
+        this.os = os;
+        this.color = color;
+        this.processor = processor;
+
+    }
+
+    public String getProcessor() {
+        return processor;
+    }
+
+    public void setProcessor(String processor) {
+        this.processor = processor;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getRam() {
+        return ram;
+    }
+
+    public void setRam(String ram) {
+        this.ram = ram;
+    }
+
+    public String getHd() {
+        return hd;
+    }
+
+    public void setHd(String hd) {
+        this.hd = hd;
+    }
+
+    public TreeMap<String, String> CreateDict() {
+        Map<String, String> item = new TreeMap<>();
+
+        item.put("model", this.model);
+        item.put("color", this.color);
+        item.put("os", this.os);
+        item.put("hd", this.hd);
+        item.put("ram", this.ram);
+        item.put("processor", this.processor);
+
+        return (TreeMap<String, String>) item;
+    }
+
+    public List<String> Test() {
+        List<String> item = new ArrayList<>();
+
+        item.add(this.model);
+        item.add(this.color);
+        item.add(this.os);
+        item.add(this.hd);
+        item.add(this.ram);
+        item.add(this.processor);
+
+        return item;
+    }
+
 }
